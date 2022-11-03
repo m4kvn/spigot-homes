5d9cee68e5409f206586ed4c22a84970e220ec10
m4kvn <fun.masahiro@gmail.com>
2022-11-04 02:46:29 +0900

    Initialize repository

---
+++ src/main/kotlin/com/github/m4kvn/spigot/playground/Main.kt
@@ -0,0 +1,15 @@
+package com.github.m4kvn.spigot.playground
+
+import org.bukkit.plugin.java.JavaPlugin
+
+@Suppress("unused")
+class Main : JavaPlugin() {
+
+    override fun onEnable() {
+        super.onEnable()
+    }
+
+    override fun onDisable() {
+        super.onDisable()
+    }
+}
