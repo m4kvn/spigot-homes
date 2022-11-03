f6a04144be602e0a5918beb2329041b04028b308
m4kvn <fun.masahiro@gmail.com>
2022-11-04 03:06:49 +0900

    Add playground command

--- src/main/kotlin/com/github/m4kvn/spigot/playground/Main.kt
+++ src/main/kotlin/com/github/m4kvn/spigot/playground/Main.kt
@@ -1,15 +1,33 @@
 package com.github.m4kvn.spigot.playground
 
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
 
 @Suppress("unused")
 class Main : JavaPlugin() {
 
     override fun onEnable() {
-        super.onEnable()
+        val playgroundCommendExecutor = PlaygroundCommendExecutor(this)
+        val playgroundCommand = getCommand("playground")
+        playgroundCommand?.setExecutor(playgroundCommendExecutor)
     }
+}
 
-    override fun onDisable() {
-        super.onDisable()
+class PlaygroundCommendExecutor(
+    private val plugin: JavaPlugin,
+) : CommandExecutor {
+
+    override fun onCommand(
+        sender: CommandSender,
+        command: Command,
+        label: String,
+        args: Array<out String>,
+    ): Boolean {
+        if (sender !is Player) return false
+        sender.sendMessage("command ok")
+        return true
     }
 }
